19e7018cebd04cc717231ed1b04f952689cbb36f
Shoyur <100440995+Shoyur@users.noreply.github.com>
2023-08-17 02:04:12 -0400

    ...

--- public-clients/scripts/Categ.js
+++
@@ -1,9 +0,0 @@
-export class Categ {
-    constructor(categ_id, categ_name, categ_sort, categ_image, categ_descr) {
-        this.categ_id = categ_id;
-        this.categ_name = categ_name;
-        this.categ_sort = categ_sort;
-        this.categ_image = categ_image;
-        this.categ_descr = categ_descr;
-      }
-}
--- public-clients/scripts/langChange.js
+++
@@ -1,4 +0,0 @@
-const language_but = document.getElementById('language_but');
-language_but.addEventListener('click', () => {
-    alert("TODO : Reload everything in english / ou en français.");
-});
