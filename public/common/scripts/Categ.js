a20863cfe48083653756aeeb166e5313cc898fe7
Shoyur <100440995+Shoyur@users.noreply.github.com>
2023-09-07 07:23:38 -0400

    ...

--- public/common/scripts/Categ.js
+++
@@ -1,7 +0,0 @@
-export class Categ {
-    constructor(categ_id, categ_name, categ_sort) {
-        this.categ_id = categ_id;
-        this.categ_name = categ_name;
-        this.categ_sort = categ_sort;
-      }
-}
--- server/config/taxes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    define('TPS', 0.05);
-    define('TVQ', 0.09875);
-
-?>
